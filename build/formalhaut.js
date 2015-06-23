119266495d39b5b5a9ecc00d6e4858954b6ca261
Etersoul <william.trans@gmail.com>
2015-06-23 11:03:31 +0700

    [Serialization] Add new serializer module to proceed file uploading

--- build/formalhaut.js
+++ build/formalhaut.js
@@ -1804,6 +1804,85 @@ var BM = {};
 (function ($, $F) {
     "use strict";
 
+    $F.fileSerialize = function (selector, arg1, arg2) {
+        var callback;
+        var returnStringify = false;
+        if (typeof arg1 === 'function') {
+            callback = arg1;
+        } else {
+            callback = arg2;
+            returnStringify = arg1;
+        }
+
+        var json = {};
+
+        var file = $(selector).find('[type=file]');
+        var fileCount = 0;
+
+
+        for (var i = 0; i < file.length; i++) {
+            var fileFiles = $(file[i]).prop('files');
+            fileCount += fileFiles.length;
+        }
+
+        // no need to proceed if no file detected
+        if (fileCount === 0) {
+            proceed(json);
+            return;
+        }
+
+        // iterate through input type file element
+        for (var i = 0; i < file.length; i++) {
+            var fileElement = file[i];
+            var fileFiles = $(fileElement).prop('files');
+
+            // iterate through multiple file
+            for (var j = 0; j < fileFiles.length; j++) {
+                var f = fileFiles[j];
+                var reader = new FileReader();
+                reader.onload = (function (arg, el) {
+                    return function (e) {
+                        var fileObject = {
+                            filename: arg.name,
+                            size: arg.size,
+                            content: e.target.result.replace(/^.*\,/g, '')
+                        };
+
+                        if (el.multiple) {
+                            if (!json[el.name]) {
+                                json[el.name] = [];
+                            }
+
+                            json[el.name].push(fileObject);
+                        } else {
+                            json[el.name] = fileObject;
+                        }
+
+                        fileCount--;
+
+                        if (fileCount === 0) {
+                            proceed(json);
+                        }
+                    };
+                })(f, fileElement);
+
+                // Read in the image file as a data URL.
+                reader.readAsDataURL(f);
+            }
+        }
+
+        function proceed(data) {
+            var ser = $F.serialize(selector);
+            ser = $.extend(ser, data);
+
+            if (returnStringify) {
+                callback(JSON.stringify(ser));
+            } else {
+                callback(ser);
+            }
+        }
+    };
+
     $F.serialize = function (selector, returnStringify) {
         returnStringify = (returnStringify == null) ? false : returnStringify;
 
@@ -1861,7 +1940,8 @@ var BM = {};
         refBefore[keyBefore] = value;
     }
 
-})(jQuery, $F);
+}
+)(jQuery, $F);
 
 /** Tabbed view system for Formalhaut **/
 (function ($, $F) {
